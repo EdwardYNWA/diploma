11c99c0816b70b5076b4719ca6ac3bee68886b0c
EdwardYNWA <121216600+EdwardYNWA@users.noreply.github.com>
2025-03-29 20:01:10 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,11 @@
+from flask import Flask
+from routes import init_routes
+
+app = Flask(__name__)
+
+# Подключаем маршруты (endpoints)
+init_routes(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
