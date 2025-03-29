36a23245ee70c4a99d61d8cabccbaa0a53de8e45
EdwardYNWA <121216600+EdwardYNWA@users.noreply.github.com>
2025-03-29 20:01:27 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    competence_level = db.Column(db.String(50), nullable=False)  # Например, "начинающий", "средний", "продвинутый"
 
