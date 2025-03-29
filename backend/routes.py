1a195b43c146bd3191f0393f01fe0c3e33fe803d
EdwardYNWA <121216600+EdwardYNWA@users.noreply.github.com>
2025-03-29 20:01:44 +0300

    Update routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -1,1 +1,13 @@
+from flask import Flask, request, jsonify
+from test_data import TEST_QUESTIONS
+
+def init_routes(app):
+    @app.route('/test', methods=['GET'])
+    def get_test():
+        return jsonify(TEST_QUESTIONS)
+
+    @app.route('/submit', methods=['POST'])
+    def submit_test():
+        data = request.json
+        return jsonify({"message": "Ответы приняты", "data": data})
 
