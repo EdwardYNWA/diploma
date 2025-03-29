8ce8a0f89359881cf352ad19487c69206d9676b1
EdwardYNWA <121216600+EdwardYNWA@users.noreply.github.com>
2025-03-29 20:05:15 +0300

    Update utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -1,19 +1,8 @@
-def classify_competence(total_points):
-    if total_points <= 30:
-        return "низкий уровень"
-    elif 31 <= total_points <= 70:
-        return "средний уровень"
+def classify_student(score):
+    if score >= 80:
+        return "Продвинутый"
+    elif score >= 50:
+        return "Средний"
     else:
-        return "высокий уровень"
-
-from test_data import test_data
-
-def evaluate_test(subject, student_answers):
-    total_points = 0
-    for answer_index, question in zip(student_answers, test_data[subject]):
-        points = question["options"][answer_index]["points"]
-        total_points += points
-    
-    competence_category = classify_competence(total_points)
-    return total_points, competence_category
+        return "Начинающий"
 
