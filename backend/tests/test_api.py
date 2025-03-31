e2d1a3a78f07995002ce534b54ce466237373bb6
EdwardYNWA <121216600+EdwardYNWA@users.noreply.github.com>
2025-03-31 20:45:04 +0300

    Update test_api.py

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -4,7 +4,9 @@ BASE_URL = "http://127.0.0.1:5000"
 
 def test_get_test():
     response = requests.get(f"{BASE_URL}/test", headers={"Content-Type": "application/json"})
-    print(response.status_code, response.text)  # Выведет код ответа и текст ошибки, если есть
+    print("Status Code:", response.status_code)
+    print("Response Text:", response.text)  # Выведет текст ошибки
     assert response.status_code == 200
 
 test_get_test()
+
